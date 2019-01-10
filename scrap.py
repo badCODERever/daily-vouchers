0e2984c14defca84209fd9e2ce73045a36b6ed52
badCODERever <ferosekhan2020@gmail.com>
2019-01-10 16:43:48 +0530

    initial commit

--- scrap.py
+++ scrap.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 import re
+from xlrd import open_workbook
+from xlutils.copy import copy
 driver = webdriver.Chrome()
-driver.get('https://www.farfetch.com/in/')
-sel_source=driver.page_source
-x = re.findall("(Up to \d\d% Off)|(\d\d% Off)|(Extra \d\d% Off)", sel_source,re.IGNORECASE)
-disct=set(x)
-for i in disct:
-	print "".join(i)
+file_name='E:\\saledata.xls'
+rb=open_workbook(file_name)
+wb=copy(rb)
+rsheet=rb.sheet_by_index(0)
+wsheet=wb.get_sheet(0)
+for i in range(rsheet.nrows):
+    offers=[]
+    url=rsheet.cell(i,0).value
+    #wsheet.write(i,1,'fetched')
+    driver.get(url)
+    sel_source=driver.page_source
+    x = re.findall("(Up to \d\d% Off)|(\d\d% Off)|(Extra \d\d% Off)", sel_source,re.IGNORECASE)
+    disct=set(x)
+    for i in disct:
+	offers.append("".join(i))
+	wsheet.write(i,1,disct)    
 driver.close()
-
+wb.save(file_name)
 
