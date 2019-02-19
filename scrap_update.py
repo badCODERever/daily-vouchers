5a1de65cde4719d825a281fa3898a2a1a732e48a
badCODERever <ferosekhan2020@gmail.com>
2019-02-19 10:29:20 +0530

    scrap_update.py

---
+++ scrap_update.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import re
+from xlrd import open_workbook
+from xlutils.copy import copy
+driver = webdriver.Chrome()
+file_name='E:\\activerstorelist.xls'
+rb=open_workbook(file_name,'wb')#open file with writemode
+wb=copy(rb)
+rsheet=rb.sheet_by_index(0)
+wsheet=wb.get_sheet(0)
+for i in range(rsheet.nrows):  
+    url=rsheet.cell(i,0).value
+    driver.get(url)
+    sel_source=driver.page_source
+    x = re.findall("(Up to \d\d% Off)|(\d\d% Off)|(Extra \d\d% Off)", sel_source,re.IGNORECASE)
+    disct=set(x)
+    li=[]
+    for j in disct:
+        li.append(''.join(j))
+    s=', '.join(str(e) for e in li)
+    wsheet.write(i,1,s)
+    wb.save(file_name)
+driver.close()
+
+
+
