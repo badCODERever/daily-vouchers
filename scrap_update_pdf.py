3967808fbb418f9b7cd853b6b4bf21a6b7064631
badCODERever <ferosekhan2020@gmail.com>
2019-07-23 11:03:41 +0530

    update

---
+++ scrap_update_pdf.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import re
+from xlrd import open_workbook
+from xlutils.copy import copy
+driver = webdriver.Chrome()
+file_name='scrap_update.py'
+rb=open_workbook(file_name,'wb')#open file with writemode
+wb=copy(rb)
+rsheet=rb.sheet_by_index(0)
+wsheet=wb.get_sheet(0)
+for i in range(1,rsheet.nrows):  
+    url=rsheet.cell(i,0).value
+    driver.get(url)
+    sel_source=driver.page_source
+    x = re.findall("(Up to \d\d% Off)|(\d\d% Off)|(Extra \d\d% Off)", sel_source,re.IGNORECASE)
+    disct=set(x)
+    li=[]
+    for j in disct:
+        li.append(''.join(j))
+    s=', '.join(str(e) for e in li)
+    print (s)
+    yvalue=rsheet.cell(i,1).value
+    wsheet.write(i,2,yvalue)
+    wsheet.write(i,1,s)
+    if s==yvalue:
+        print("No Changes in Offer")
+        wsheet.write(i,3,"No Change")
+    else:
+        print("Offer got changed")
+        wsheet.write(i, 3, "Change in Offer")
+    wb.save(file_name)
+driver.close()
+
+
+
